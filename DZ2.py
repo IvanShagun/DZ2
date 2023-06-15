891844ee1fc9b2f82d7be428143d041e57f2d7fd
Ivan Shagun <136569386+IvanShagun@users.noreply.github.com>
2023-06-15 17:17:38 +0400

    Update DZ2.py

--- DZ2.py
+++ DZ2.py
@@ -36,35 +36,3 @@ for i in range(d):
         print(x, y)
     elif y >= 1002:
         print("Не соответсвует условию")
-
-
-# n = int(input())
-# count_zero = 0
-# count_one = 0
-# for i in range(n):
-#     x = int(input())
-#     if x == 0:
-#         count_zero += 1
-#     else:
-#         count_one += 1
-# if count_one > count_zero:
-#     print(count_zero)
-# else:
-#     print(count_one)
-
-    
-    
-# n = int(input())
-# i = 0
-# while 2 ** i <= n:
-#     print(2 ** i)
-#     i += 1
-
-
-
-# x = int(input())
-# y = int(input())
-# for i in range(x):
-#     for j in range(y):
-#         if x == i + j and y == i * j:
-#             print(i, j)
